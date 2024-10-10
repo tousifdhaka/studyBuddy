fbd526f9c015423cb4556c18c387bb22f399c1e9
Ansh Naikele <ansh.naikele2003@gmail.com>
2024-10-10 00:25:36 -0500

    setup frontend and backend servers

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const jose = require('jose');
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING)
+
+app.listen(1337, () => {
+    console.log('Server is running on port 1337');
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+const App = () => {
+    return(
+        <div>
+            <Router>
+                <Routes>
+                    
+                </Routes>
+            </Router>
+
+        </div>
+    );
+}
+
+export default App;
